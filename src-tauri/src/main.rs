c3929e7353a6b5e9af69c90acb8786dbf5437850
dnettoRaw <me@dnetto.dev>
2022-09-20 10:18:09 +0200

    automated commit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 /*  ##   ## ##   ##   F: main.rs                              */
 /*       ## ##                                                */
 /*                    C: 2022/06/15 16:11:27 by:dnettoRaw     */
-/*  ##   ## ##   ##   U: 2022/06/15 16:51:42 by:dnettoRaw     */
+/*  ##   ## ##   ##   U: 2022/09/20 09:47:41 by:dnettoRaw     */
 /*    ###########                                             */
 
 
@@ -14,14 +14,27 @@
 
 mod menu_toolbar;
 mod function;
+use tauri::WindowBuilder;
+
 use crate::menu_toolbar::menu::{dr_menu, dr_event};
 use crate::function::button_test;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![button_test::my_button])
-    .menu(dr_menu())
-    .on_menu_event(dr_event)
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  .setup(|app|{
+    WindowBuilder::new(
+      app,
+      "main".to_string(),
+      tauri::WindowUrl::App("index.html".into()),
+    )
+    .title("little App")
+    .resizable(false)
+    .min_inner_size(700.0, 600.0)
+    .build()?;
+    Ok(())
+  }).invoke_handler(tauri::generate_handler![button_test::my_button])
+  .menu(dr_menu())
+  .on_menu_event(dr_event)
+  .run(tauri::generate_context!("tauri.conf.json"))
+  .expect("error while running tauri application");
 }
--- src-tauri/src/menu_toolbar/windows.rs
+++ src-tauri/src/menu_toolbar/windows.rs
@@ -1,24 +1,45 @@
 /*      #######                                               */
 /*   ###       ###                                            */
-/*  ##   ## ##   ##   F: menu_windows.rs                      */
+/*  ##   ## ##   ##   F: windows.rs                           */
 /*       ## ##                                                */
 /*                    C: 2022/06/02 16:38:13 by:dnettoRaw     */
-/*  ##   ## ##   ##   U: 2022/06/16 09:05:33 by:dnettoRaw     */
+/*  ##   ## ##   ##   U: 2022/09/20 09:55:23 by:dnettoRaw     */
 /*    ###########                                             */
 
-#![allow(dead_code)]
-
-use tauri::{Menu};
+#[cfg(windows)]
+use tauri::{
+ CustomMenuItem, Menu, MenuItem
+};
 
 pub fn get_my_app() -> Menu {
-    Menu::with_items([])
-  }
-  pub fn get_file() -> Menu {
-    Menu::with_items([])
-  }
-  pub fn get_edit() -> Menu {
-    Menu::with_items([])
-  }
-  pub fn get_help() -> Menu {
-    Menu::with_items([])
-  }
+  Menu::with_items([
+    CustomMenuItem::new("20", "settings").into(),
+    MenuItem::Hide.into(),
+    MenuItem::Separator.into(),
+    MenuItem::Quit.into(),
+  ])
+}
+pub fn get_file() -> Menu {
+  Menu::with_items([])
+}
+pub fn get_edit() -> Menu {
+  Menu::with_items([
+    MenuItem::Undo.into(),
+    MenuItem::Redo.into(),
+    MenuItem::Separator.into(),
+    MenuItem::Cut.into(),
+    MenuItem::Copy.into(),
+    MenuItem::Paste.into(),
+    MenuItem::Separator.into(),
+    MenuItem::SelectAll.into(),
+  ])
+}
+pub fn get_help() -> Menu {
+  Menu::with_items([
+    MenuItem::Separator.into(),
+    //  firts is Id this need to be same in your functions, secound is the name displayed
+    CustomMenuItem::new("01", "dnetto github").into(),
+    CustomMenuItem::new("02", "dnetto site").into(),
+    CustomMenuItem::new("03", "tauri github").into(),
+  ])
+}
